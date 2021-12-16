b71a9942f95526631f44adb0843b7817a3beb479
Milton Rincón <milton@cosmicgo.co>
2021-12-16 02:59:44 -0500

    Main contract

--- contracts/MetasaursPunks.sol
+++ contracts/MetasaursPunks.sol
@@ -137,6 +137,20 @@ contract MetasaursPunks is ERC721, ERC721Enumerable, Ownable {
 		return msg.value >= totalValue;
 	}
 	
+	function tokensOfOwner(address _owner) external view returns (uint256[] memory) {
+		uint256 tokenCount = balanceOf(_owner);
+		if (tokenCount == 0) {
+			return new uint256[](0);
+		} else {
+			uint256[] memory result = new uint256[](tokenCount);
+			uint256 index;
+			for (index = 0; index < tokenCount; index++) {
+				result[index] = tokenOfOwnerByIndex(_owner, index);
+			}
+			return result;
+		}
+	}
+	
 	function withdraw() external onlyOwner {
 		uint balance = address(this).balance;
 		payable(msg.sender).transfer(balance);
